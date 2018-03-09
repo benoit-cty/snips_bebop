e34e0a00dbcfaff5d6bf3846eff8f4ea2f5127ac
Benoit <benoit.courty.ext@francetv.fr>
2018-03-09 18:36:28 +0100

    MQTT

--- snips_bebop.py
+++ snips_bebop.py
@@ -14,26 +14,26 @@ if with_mqtt:_client = mqtt.Client()
 # Subscribe to the important messages
 def on_connect(client, userdata, flags, rc):
     mqtt_client.subscribe('hermes/intent/lightsTurnOnSet')
-    mqtt_client.subscribe('hermes/intent/lightsTurnOff')
-    mqtt_client.subscribe('hermes/intent/lightsTurnUp')
-    mqtt_client.subscribe('hermes/intent/lightsTurnDown')
+
 # Process a message as it arrives
 def on_message(client, userdata, msg):
-    if msg.topic == 'hermes/intent/lightsTurnOnSet':
-        action = "on"
-    elif msg.topic == 'hermes/intent/lightsTurnOff':
-        action = "off"
-    elif msg.topic == 'hermes/intent/lightsTurnUp':
-        action = "up"
-    elif msg.topic == 'hermes/intent/lightsTurnDown':
-        action = "down"
-    slots = parse_slots(msg)
+    # Read Snips Payload
+    slots = parse_slots(json.loads(msg.payload))
     session_id = parse_session_id(msg)
-    if 'house_room' not in slots:
-        say(session_id, "I don't know where I should turn {} the lights".format(action))
+    if 'Action' not in slots:
+        say(session_id, "I don't know where I should go.")
     else:
-        room = slots['house_room']
-        say(session_id, 'Turning {} the lights in the {}'.format(action, room))
+        action = slots['Action']
+        say(session_id, 'OK, I will {} my lord.'.format(action))
+        make_move(action)
+
+
+def parse_session_id(msg):
+    '''
+    Extract the session id from the message
+    '''
+    data = json.loads(msg.payload)
+    return data['sessionId']
 
 def say(session_id, text):
     '''
